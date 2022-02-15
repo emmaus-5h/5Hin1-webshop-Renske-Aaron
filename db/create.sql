7fc88b00052fcda36954d62b6b2fb8310b51d7e0
renskehartman <74650402+renskehartman@users.noreply.github.com>
2022-02-15 17:38:29 +0000

    prijzen aangepast

--- db/create.sql
+++ db/create.sql
@@ -4,6 +4,7 @@
 
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
@@ -21,15 +22,18 @@ CREATE TABLE products (
 --
 
 insert into products (name, description, code, price) values ('Poster Vlinders', 'Een zeer mooie poster van vlinders die vliegen, cursus id.', '816905633-0', 5);
-insert into products (name, description, code, price) values ('Poster 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('poster 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('poster 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('poster 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('poster 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-insert into products (name, description, code, price) values ('poster 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
-insert into products (name, description, code, price) values ('poster 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
-insert into products (name, description, code, price) values ('poster 9', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
-insert into products (name, description, code, price) values ('poster 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Poster 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 4.50);
+insert into products (name, description, code, price) values ('poster 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 6);
+insert into products (name, description, code, price) values ('poster 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 5.50);
+insert into products (name, description, code, price) values ('poster 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 5);
+insert into products (name, description, code, price) values ('poster 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 6.50);
+insert into products (name, description, code, price) values ('poster 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 5.25);
+
+insert into products (name, description, code, price) values ('poster 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 4.75);
+
+insert into products (name, description, code, price) values ('poster 9', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 5.25);
+
+insert into products (name, description, code, price) values ('poster 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 5.75);
