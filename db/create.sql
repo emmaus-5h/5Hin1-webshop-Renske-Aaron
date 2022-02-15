82313665e239841421164495d2a502fd0b2f40e1
renskehartman <74650402+renskehartman@users.noreply.github.com>
2022-02-15 19:00:52 +0000

    eigenschap "materiaal" toegevoegd

--- db/create.sql
+++ db/create.sql
@@ -9,6 +9,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   grootte TEXT,
+  materiaal TEXT,
   price NUMERIC(10, 2)
   
 );
@@ -23,23 +24,23 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, grootte, price) values ('Poster Vlinders', 'Een zeer mooie poster van vlinders die vliegen', '816905633-0', 'test', 5 );
+insert into products (name, description, code, grootte, materiaal,price) values ('Poster Vlinders', 'Een zeer mooie poster van vlinders die vliegen', '816905633-0', 'test', 'papier', 5 );
 
 
-insert into products (name, description, code, grootte, price) values ('Poster 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3','test', 4.50);
+insert into products (name, description, code, grootte,  materiaal, price) values ('Poster 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3','test', 'papier', 4.50);
 
-insert into products (name, description, code, grootte, price) values ('poster 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 'test', 6);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 'test', 'papier', 6);
 
- insert into products (name, description, code, grootte, price) values ('poster 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7','test', 5.50);
+ insert into products (name, description, code, grootte,  materiaal, price) values ('poster 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7','test', 'papier', 5.50);
 
-insert into products (name, description, code, grootte, price) values ('poster 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6','test', 5);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6','test', 'papier', 5);
 
-insert into products (name, description, code, grootte, price) values ('poster 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 6.50);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 'papier', 6.50);
 
-insert into products (name, description, code, grootte, price) values ('poster 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 5.25);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 'papier', 5.25);
 
-insert into products (name, description, code, grootte, price) values ('poster 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 4.75);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 'papier', 4.75);
 
-insert into products (name, description, code, grootte, price) values ('poster 9', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 5.25);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 9', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 'papier', 5.25);
 
-insert into products (name, description, code, grootte, price) values ('poster 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 5.75);
+insert into products (name, description, code, grootte,  materiaal, price) values ('poster 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','test', 'papier', 5.75);
