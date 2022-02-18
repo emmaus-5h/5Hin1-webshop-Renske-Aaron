5243cba903f5ed6e8f09a0468d712f9e9873a611
renskehartman <74650402+renskehartman@users.noreply.github.com>
2022-02-18 22:28:57 +0000

    tabel opgelost, grootte van de artiekelen aangepast zodat het er netter uitziet

--- db/create.sql
+++ db/create.sql
@@ -10,14 +10,14 @@ CREATE TABLE products (
   description TEXT,
   grootte TEXT,
   materiaal TEXT,
-  price NUMERIC(10, 2)
-  
+  price NUMERIC(10, 2),
+  soort_id INTEGER
+
 );
 
 
 CREATE TABLE soort (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  
  name VARCHAR(255),
   
 );
@@ -30,7 +30,7 @@ CREATE TABLE soort (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, grootte, materiaal, price, soort_id) values ('Poster Molen', 'Een zeer mooie poster van vlinders die vliegen', '816905633-0', '  8,3 x 11,7', '  Mat Photo Papier', 5 , 1);
+insert into products (name, description, code, grootte, materiaal, price, soort_id) values ('Poster Molen', 'Een zeer mooie poster van vlinders die vliegen', '816905633-0', '  8,3 x 11,7', '  Mat Photo Papier', 5, 1);
 
 
 insert into products (name, description, code, grootte,  materiaal, price, soort_id ) values ('Poster Waterval', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3','  8,3 x 11,7', '  Mat Photo Papier', 4.50, 2);
@@ -49,7 +49,7 @@ insert into products (name, description, code, grootte,  materiaal, price, soort
 
 insert into products (name, description, code, grootte,  materiaal, price, soort_id) values ('Poster Graffiti', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','  11,7 x 16,5', '  Glanzend Papier', 5.25, 1);
 
-insert into products (name, description, code, grootte,  materiaal, soort_id) values ('Poster De Verwoeste Stad', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','  11,7 x 16,5', '  Textiel', 5.75, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id) values ('Poster De Verwoeste Stad', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7','  11,7 x 16,5', '  Textiel', 5.75, 2);
 
 
 
