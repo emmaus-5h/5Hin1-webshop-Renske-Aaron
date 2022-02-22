2d5c228f2a15d37796425ecae23b0e633a26b827
Aron <65195141+Aron156@users.noreply.github.com>
2022-02-22 10:30:26 +0000

    Kleine dingen in sql

--- db/create.sql
+++ db/create.sql
@@ -22,9 +22,9 @@ CREATE TABLE soort (
   
 );
 
-CREATE TABLE iets (
+CREATE TABLE levertijd (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
- iets TEXT,
+ Levertijd NUMERIC(10, 2),
 
 );
 --
@@ -36,12 +36,12 @@ CREATE TABLE iets (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, grootte, materiaal, price, soort_id) values ('Poster Molen', 'Een zeer mooie poster van vlinders die vliegen', '816905633-0', '  8,3 x 11,7', '  Mat Photo Papier', 5, 1);
+insert into products (name, description, code, grootte, materiaal, price, soort_id) values ('Poster Molen', 'Een zeer mooie poster van een molen.', '816905633-0', '  8,3 x 11,7', '  Mat Photo Papier', 5, 1);
 
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id ) values ('Poster Waterval', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3','  8,3 x 11,7', '  Mat Photo Papier', 4.50, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id ) values ('Poster Waterval', 'Een poster van een Waterval in een natuur gebied.', '077030122-3','  8,3 x 11,7', '  Mat Photo Papier', 4.50, 2);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id) values ('Poster Italiaanse Kerk', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', '  16,5 x 23,4', '  Textiel', 6, 1);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id) values ('Poster Italiaanse Kerk', 'Een poster van een Italiaanse Kerk.', '445924201-X', '  16,5 x 23,4', '  Textiel', 6, 1);
 
  insert into products (name, description, code, grootte,  materiaal, price, soort_id) values ('Poster Stad', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7','  11,7 x 16,5', '  Glanzend Papier', 5.50, 2 );
 
