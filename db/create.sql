998e98b30ebf016b38214d621e469db18f03bd71
renskehartman <74650402+renskehartman@users.noreply.github.com>
2022-03-04 18:54:56 +0000

    waarden ingevoerd tabel frame

--- db/create.sql
+++ db/create.sql
@@ -49,26 +49,26 @@ CREATE TABLE frame (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, grootte, materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Molen', 'Een zeer mooie poster van een molen. ', '816905633-0',' 8,3 x 11,7', '  Mat Photo Papier', 5, 2, 2, 1);
+insert into products (name, description, code, grootte, materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Molen', 'Een zeer mooie poster van een molen. ', '816905633-0',' 8,3 x 11,7', '  Mat Photo Papier', 5, 2, 2, 1, 2);
 
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Waterval', 'Een poster van een Waterval in een natuur gebied.', '077030122-3','  8,3 x 11,7', '  Mat Photo Papier', 4.50, 1, 2, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Waterval', 'Een poster van een Waterval in een natuur gebied.', '077030122-3','  8,3 x 11,7', '  Mat Photo Papier', 4.50, 1, 2, 2, 1);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Italiaanse Kerk', 'Een poster van een Italiaanse Kerk.', '445924201-X', '  16,5 x 23,4', '  Textiel', 6, 3, 3, 3);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Italiaanse Kerk', 'Een poster van een Italiaanse Kerk.', '445924201-X', '  16,5 x 23,4', '  Textiel', 6, 3, 3, 3, 1);
 
- insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Stad', 'Een prachtige poster van een gebouw in de stad Dusseldorf in de nacht.', '693155505-7','  11,7 x 16,5', '  Glanzend Papier', 5.50, 3, 2, 2 );
+ insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Stad', 'Een prachtige poster van een gebouw in de stad Dusseldorf in de nacht.', '693155505-7','  11,7 x 16,5', '  Glanzend Papier', 5.50, 3, 2, 2 , 2);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Zonsondergang', 'Een rustige poster die de zonsondergang weergeeft.', '686928463-6','  8,3 x 11,7', '  Mat Photo Papier', 5 , 1, 3, 1);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Zonsondergang', 'Een rustige poster die de zonsondergang weergeeft.', '686928463-6','  8,3 x 11,7', '  Mat Photo Papier', 5 , 1, 3, 1, 3);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Markthal Rotterdam', 'Een poster van een van de bekendste plekken van Rotterdam, de markthal!', '492662523-7','  16,5 x 23,4', '  Textiel', 6.50, 3, 1, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Markthal Rotterdam', 'Een poster van een van de bekendste plekken van Rotterdam, de markthal!', '492662523-7','  16,5 x 23,4', '  Textiel', 6.50, 3, 1, 2, 2);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Italie', 'Een poster van het gezelligste stadje van Italie, Lido di Spina.', '492662523-7','  11,7 x 16,5', '  Glanzend Papier', 5.25, 2, 1, 3);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Italie', 'Een poster van het gezelligste stadje van Italie, Lido di Spina.', '492662523-7','  11,7 x 16,5', '  Glanzend Papier', 5.25, 2, 1, 3, 3);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Feyenoord', 'Een poster van de Kuip, perfect voor Feyenoord-fans.', '492662523-7','  8,3 x 11,7', '  Mat Photo Papier', 4.75, 3, 1, 1);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Feyenoord', 'Een poster van de Kuip, perfect voor Feyenoord-fans.', '492662523-7','  8,3 x 11,7', '  Mat Photo Papier', 4.75, 3, 1, 1, 2);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster Graffiti', 'Een poster van grafitti op een muur in Dusseldorf.', '492662523-7','  11,7 x 16,5', '  Glanzend Papier', 5.25, 2, 1, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster Graffiti', 'Een poster van grafitti op een muur in Dusseldorf.', '492662523-7','  11,7 x 16,5', '  Glanzend Papier', 5.25, 2, 1, 2, 1);
 
-insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id) values ('Poster De Verwoeste Stad', 'Een poster van een belangrijk beeld van Rotterdam, zadkine.', '492662523-7','  11,7 x 16,5', '  Textiel',5.75, 3, 3, 2);
+insert into products (name, description, code, grootte,  materiaal, price, soort_id, doelgroep_id, levertijd_id, frame_id) values ('Poster De Verwoeste Stad', 'Een poster van een belangrijk beeld van Rotterdam, zadkine.', '492662523-7','  11,7 x 16,5', '  Textiel',5.75, 3, 3, 2, 3);
 
 
 
